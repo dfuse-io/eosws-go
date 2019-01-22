7a4ffd8ea58f9da700d1b56a440865e5bb26dbbe
shrimp <liu.shrimp@gmail.com>
2019-01-22 15:46:37 +0800

    fix

--- head.go
+++ head.go
@@ -13,7 +13,7 @@ type HeadInfo struct {
 		LastIrreversibleBlockNum	uint32		`json:"last_irreversible_block_num"`
 		LastIrreversibleBlockId 	string		`json:"last_irreversible_block_id"`
 		HeadBlockNum				uint32 		`json:"head_block_num"`
-		HeadBlockId 				string 		`json:"head_block_id"`
+		HeadBlockID 				string 		`json:"head_block_id"`
 		HeadBlockTime				time.Time	`json:"head_block_time"`
 		HeadBlockProducer 			string 		`json:"head_block_producer"`
 	} `json:"data"`
