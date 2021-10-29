1e40901637841cf948180e1a53843db071ba8378
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-10-29 09:46:51 -0400

    Added missing family & action to v1/RAMOp

--- mdl/v1/ramop.go
+++ mdl/v1/ramop.go
@@ -2,6 +2,8 @@ package mdl
 
 type RAMOp struct {
 	Operation   string `json:"op"`
+	Family      string `json:"family"`
+	Action      string `json:"action"`
 	ActionIndex int    `json:"action_idx"`
 	Payer       string `json:"payer"`
 	Delta       int64  `json:"delta"`
